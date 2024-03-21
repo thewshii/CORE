f46f2f034b82976d010c38e2e9eb595f1d20c93e
Wayne <thewshii@gmail.com>
2024-03-20 23:25:05 -0400

    have some more things working..
    added direction to driver map view
    useref for automatic zooming in on driver map view

--- src/screens/driver/DriverMapView.js
+++ src/screens/driver/DriverMapView.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { View, StyleSheet, Alert, Text, TouchableOpacity } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 import tw from 'tailwind-react-native-classnames';
 import supabase from '../../supabase/supabaseClient';
@@ -8,61 +8,94 @@ import { useSelector } from 'react-redux';
 import { selectUser } from '../../slices/userSlice';
 import { useNavigation } from '@react-navigation/native';
 import { Icon } from 'react-native-elements';
+import MapViewDirections from 'react-native-maps-directions';
+import Constants from 'expo-constants';
 
 function DriverMapView() {
   const navigation = useNavigation();
   const userID = useSelector(selectUser);
   const [isOnline, setIsOnline] = useState(false);
   const [rideRequests, setRideRequests] = useState([]);
+  const [driverLocation, setDriverLocation] = useState(null);
+  const [selectedRideRequest, setSelectedRideRequest] = useState(null);
+  const GOOGLE_MAPS_APIKEY = Constants.expoConfig.extra.googleApiKey;
+  const mapRef = useRef(null);
 
   useEffect(() => {
-    const subscription = supabase
-      .channel('public-ride_bookings')
-      .on('postgres_changes', { event: 'INSERT', schema: 'public' }, payload => {
-        console.log('New ride request:', payload.new);
-        // Filter the requests to only show those relevant to this driver if necessary
-        // For example, if you have a logic to assign requests to specific drivers
-        setRideRequests(prev => [...prev, payload.new]);
-      })
-      .subscribe();
+    const getDriverLocation = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('Permission denied', 'You need to grant location permission to get the driver\'s location.');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setDriverLocation(location.coords);
+    };
+
+    getDriverLocation();
+  }, []);
+
+  useEffect(() => {
+    let subscription;
+    if (isOnline) {
+      subscription = supabase
+        .channel('public-ride_bookings')
+        .on('postgres_changes', { event: 'INSERT', schema: 'public' }, payload => {
+          console.log('New ride request received.');
+          setRideRequests(prev => [...prev, payload.new]);
+        })
+        .subscribe();
+    }
 
     return () => {
-      supabase.removeSubscription(subscription);
+      if (subscription) {
+        supabase.removeChannel(subscription);
+      }
     };
-  }, []);
+  }, [isOnline]);
 
   const acceptRide = async (rideId) => {
     const { error } = await supabase
       .from('ride_bookings')
-      .update({ status: 'accepted' })
+      .update({ status: 'accepted', driver_id: userID })
       .eq('id', rideId);
 
     if (error) {
       console.error('Error updating ride status:', error.message);
-    } else {
-      // Optionally filter out the accepted request from local state
-      setRideRequests(prevRequests => prevRequests.filter(request => request.id !== rideId));
-      Alert.alert('Ride Accepted', 'You have accepted the ride.');
+      return;
     }
+
+    const ride = rideRequests.find(request => request.id === rideId);
+    setSelectedRideRequest(ride);
+    setRideRequests(prevRequests => prevRequests.filter(request => request.id !== rideId));
   };
 
   const declineRide = (rideId) => {
     setRideRequests(prevRequests => prevRequests.filter(request => request.id !== rideId));
-    Alert.alert('Ride Declined', 'You have declined the ride.');
-
+    if (selectedRideRequest && selectedRideRequest.id === rideId) {
+      setSelectedRideRequest(null);
+    }
   };
 
-  async function toggleOnlineStatus() {
-    setIsOnline(!isOnline);
-    // Here you would also update the driver's status in your database
-  }
+  const handleGoBack = () => {
+    navigation.goBack();
+  };
 
-  const handleGoBack = () => navigation.goBack();
+  
+
+  const toggleOnlineStatus = async () => {
+    setIsOnline(prev => !prev);
+    await supabase
+      .from('drivers')
+      .update({ onlineStatus: isOnline ? 'true' : 'false' })
+      .eq('uuid', userID);
+  };
 
   return (
     <View style={tw`h-full`}>
       <MapView
-        ref={mapRef => mapRef}
+        ref={mapRef}
         style={tw`w-full h-full`}
         provider="google"
         showsUserLocation={true}
@@ -73,7 +106,66 @@ function DriverMapView() {
           latitudeDelta: 1,
           longitudeDelta: 1,
         }}
-      />
+      >
+        {selectedRideRequest && driverLocation && (
+          <MapViewDirections
+            origin={driverLocation}
+            destination={selectedRideRequest.pickupLocation} // Assuming this is a valid coordinate
+            apikey={GOOGLE_MAPS_APIKEY}
+            strokeWidth={3}
+            strokeColor="black"
+            onReady={result => {
+              mapRef.current.fitToCoordinates(result.coordinates, {
+                edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+                animated: true,
+              });
+            }}
+            onError={(errorMessage) => {
+              console.error('GMAPS route request error:', errorMessage);
+              Alert.alert("Route Error", "An error occurred while trying to find a route.");
+            }}
+          />
+        )}
+        {driverLocation && (
+          <Marker
+            coordinate={{ latitude: driverLocation.latitude, longitude: driverLocation.longitude }}
+            title="Driver"
+            description="You are here"
+          />
+        )}
+
+        {rideRequests.map((request, index) => (
+          <Marker
+            key={index}
+            coordinate={{ latitude: request.pickupLocation.latitude, longitude: request.pickupLocation.longitude }}
+            title="New Ride Request"
+            description="This is where you need to go"
+            pinColor="blue"
+          />
+        ))
+        }
+        
+
+        {selectedRideRequest && (
+          <Marker
+            coordinate={{ latitude: selectedRideRequest.pickupLocation.latitude, longitude: selectedRideRequest.pickupLocation.longitude }}
+            title="Pickup Location"
+            description="This is where you need to go"
+            pinColor="green"
+          />
+        )}
+
+        {selectedRideRequest && (
+          <Marker
+            coordinate={{ latitude: selectedRideRequest.dropoffLocation.latitude, longitude: selectedRideRequest.dropoffLocation.longitude }}
+            title="Dropoff Location"
+            description="This is where you need to take the passenger"
+            pinColor="red"
+          />
+        )}
+
+        
+      </MapView>
 
       {rideRequests.map((request, index) => (
         <View key={index} style={styles.rideRequestContainer}>
@@ -91,8 +183,7 @@ function DriverMapView() {
 
       <TouchableOpacity
         onPress={toggleOnlineStatus}
-        style={[tw`absolute bottom-12 self-center w-24 h-24 rounded-full justify-center items-center`,
-          { backgroundColor: isOnline ? 'red' : 'green' }]}>
+        style={[tw`absolute bottom-12 self-center w-24 h-24 rounded-full justify-center items-center`, { backgroundColor: isOnline ? 'red' : 'green' }]}>
         <Text style={tw`text-white text-center font-bold`}>{isOnline ? "Go Offline" : "Go Online"}</Text>
       </TouchableOpacity>
 
@@ -135,3 +226,4 @@ const styles = StyleSheet.create({
 });
 
 export default DriverMapView;
+                
